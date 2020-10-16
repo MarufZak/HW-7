34e44c8237f03049b2816a646e7259034910949f
Maruf <maruf@maruf.maruf>
2020-10-16 22:20:17 +0500

    Сделал дз

---
+++ script.js
@@ -0,0 +1,40 @@
+var name = prompt('Введите ваше имя');
+
+console.log("Меня зовут " + name);
+
+var age = prompt('Сколько вам лет , ' + name + " ака ?");
+
+console.log("Мне " + age + " лет");
+
+var plus = prompt('5 + 3 = ?');
+
+console.log("5 + 3 = 8   " + "Ваш ответ‚ " + plus);
+
+
+
+var minus = prompt('5 - 3 = ?');
+
+console.log("5 - 3 = 2   " + "Ваш ответ‚ " + minus);
+
+
+var mult = prompt('5 * 3 = ?');
+
+console.log("5 * 3 = 15   " + "Ваш ответ‚ " + mult);
+
+
+
+var div = prompt('6 / 3 = ?');
+
+console.log("6 / 3 = 2   " + "Ваш ответ‚ " + div);
+
+
+var left = prompt('7 % 3 = ?');
+
+console.log("7 % 3 = 1   " + "Ваш ответ‚ " + left);
+
+alert('Зайдите в консоль , чтобы увидеть результат');
+
+console.log("Неплохо");
+
+
+
